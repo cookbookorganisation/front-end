ea9839f94a1a220d29505e4ace77db2676f340d2
Joey Nevarez <josephnevarez12@gmail.com>
2020-09-27 11:07:40 -0700

    more or less finishes styling for signup form

--- src/components/forms/SignUp.js
+++ src/components/forms/SignUp.js
@@ -1,148 +1,141 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-
+import { useHistory } from 'react-router-dom';
+import { Avatar, Button, Link, Grid, CssBaseline, TextField, Box, Typography, Container, makeStyles } from "@material-ui/core";
 // import SVG JS component
 import CookBookIcon from "../../images/icons/cookBookIcon.js";
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        CookBook Organisation
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://material-ui.com/">
+                CookBook Organisation
+            </Link>{" "}
+            {new Date().getFullYear()}{"."}
+        </Typography>
+    );
+};
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: "#F9F9F9",
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    backgroundColor: "#D96704",
-    "&:hover": {
-      backgroundColor: "#000",
+    paper: {
+        marginTop: theme.spacing(12),
+        padding: theme.spacing(3),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        border: "1px solid black",
+        boxShadow: "3px 3px 8px #888888",
+        backgroundColor: "#f8f8ff",
+        borderRadius: "5px"
     },
-  },
+    avatar: {
+        margin: theme.spacing(-9, 1, 1, 1),
+        padding: theme.spacing(3),
+        backgroundColor: "#f8f8ff",
+        height: "100px",
+        width: "100px",
+        borderTop: "1px solid black",
+    },
+    title: {
+        fontWeight: "600",
+        fontSize: "1.9rem",
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(6, 0, 0),
+        padding: theme.spacing(0),
+        width: "80%",
+        fontWeight: "900",
+        fontSize: "1.2rem",
+        letterSpacing: ".1rem",
+        backgroundColor: "#D96704",
+        "&:hover": {
+        backgroundColor: "#000",
+        },
+    },
 }));
 
 const SignUp = () => {
-  const classes = useStyles();
+    const classes = useStyles();
+    const history = useHistory();
 
-  return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <CookBookIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <form className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign Up
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
+    return (
+        <div className="formBackground">
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                    <div className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <CookBookIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5" className={classes.title}>
+                            Sign up
+                        </Typography>
+                        <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="fname"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus/>
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="lname"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                        fullWidth
+                                        id="email"
+                                        label="Email"
+                                        name="email"
+                                        autoComplete="email"/>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"/>
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}>
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <p onClick={()=>history.push('/login')} variant="body2" style={{ color: "#590E07" }}>
+                                    Already have an account? Log In
+                                </p>
+                            </Grid>
+                        </Grid>
+                    </form>
+                    <Box mt={5}>
+                        <Copyright />
+                    </Box>
+                </div>
+            </Container>
         </div>
-        <Box mt={5}>
-          <Copyright />
-        </Box>
-      </Container>
-    </div>
-  );
+    );
 };
 
 export default SignUp;
